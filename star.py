a456af98f80da8587e47f9e33df92ee81c955a70
DHANARAJ <dhanarajappu456@gmail.com>
2020-03-16 17:44:38 +0530

    star created

---
+++ star.py
@@ -0,0 +1,23 @@
+import turtle
+
+p=turtle.Turtle()
+p.speed(1)
+
+p.left(70)
+p.forward(120)
+p.right(150)
+p.forward(200)
+p.right(180)
+p.left(30)
+
+p.forward(175)
+p.right(130)
+p.forward(140)
+p.right(135)
+p.forward(185)
+p.right(154.5)
+p.forward(100)
+
+turtle.done()
+
+print("CREATED BY DHANARAJ S")
